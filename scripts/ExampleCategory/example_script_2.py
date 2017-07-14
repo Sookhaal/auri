60661371a7cbe32a8a1495258d0ffbb501cc5f41
Nelios <sookhaal@gmail.com>
2017-07-14 09:26:05 +0200

    Update script module minimum required code

--- scripts/ExampleCategory/example_script_2.py
+++ scripts/ExampleCategory/example_script_2.py
@@ -1,4 +1,4 @@
-from auri.auri_lib import push_button, AuriScriptView, AuriScriptController
+from auri.auri_lib import push_button, AuriScriptView, AuriScriptController, AuriScriptModel
 from PySide2 import QtGui, QtWidgets
 
 
@@ -6,16 +6,30 @@ class View(AuriScriptView):
     def __init__(self, *args, **kwargs):
         super(View, self).__init__(*args, **kwargs)
 
+    def set_model(self):
+        self.model = Model()
+
+    def set_controller(self):
+        self.ctrl = Controller(self.model)
+
     def setup_ui(self):
         pass
 
-    def set_controller(self):
-        self.ctrl = Controller()
-
 
 class Controller(AuriScriptController):
+    def __init__(self, model):
+        """
+
+        Args:
+            model (Model):
+        """
+        self.model = model
+        AuriScriptController.__init__(self)
+
     def execute(self):
         print __name__
 
+
+class Model(AuriScriptModel):
     def __init__(self):
-        AuriScriptController.__init__(self)
+        AuriScriptModel.__init__(self)
